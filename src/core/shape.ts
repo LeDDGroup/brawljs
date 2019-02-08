4da6444165651a478c6f0a3da2192e4af4fb18db
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-02-07 19:56:07 -0500

    remove unused code

--- src/core/shape.ts
+++ src/core/shape.ts
@@ -28,29 +28,4 @@ export class Circle {
 export class Rect {
   position: Point = new Point();
   size: Point = new Point();
-  collides(other: Rect): boolean {
-    return (
-      segmentCollides(
-        this.position.x,
-        this.position.x + this.size.x - 1,
-        other.position.x,
-        other.position.x + other.size.x - 1
-      ) &&
-      segmentCollides(
-        this.position.y,
-        this.position.y + this.size.y - 1,
-        other.position.y,
-        other.position.y + other.size.y - 1
-      )
-    );
-  }
-}
-
-function segmentCollides(
-  x1: number,
-  x2: number,
-  y1: number,
-  y2: number
-): boolean {
-  return (x1 >= y1 && x1 <= y2) || (x2 >= y1 && x2 <= y2);
 }
